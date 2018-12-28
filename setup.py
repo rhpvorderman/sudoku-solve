a0c0ccad662b2b66804a50da65252d3fa1abf7f8
Ruben Vorderman <RubenVorderman@xs4all.nl>
2018-12-28 10:09:29 +0100

    add correct license

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2018 Ruben Vorderman
-# This file is part of pytest-workflow
+# This file is part of sudoku-solve
 #
 # pytest-workflow is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
